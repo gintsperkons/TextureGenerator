04640e60cd36c9212ff22a7b2560f296994d61fd
Gints Kristaps Perkons <gintskristaps13@gmail.com>
2024-07-18 00:23:25 +0300

    setup Update

--- Scripts/Python/InstallPremake.py
+++ Scripts/Python/InstallPremake.py
@@ -4,20 +4,26 @@ sys.path.append(f'Scripts/Python')
 import Defines
 import Utils
 import zipfile
+import tarfile
 
 
-tempPremakeArchive = "Vendor/Binary/Temp/Premake.zip"
+tempPremakeArchive = "Vendor/Binary/Temp/Premake"
 
-premakeURL = {"nt":"https://github.com/premake/premake-core/releases/download/v5.0.0-beta2/premake-5.0.0-beta2-windows.zip"
-              }
+premakeURL = {"nt":"https://github.com/premake/premake-core/releases/download/v5.0.0-beta2/premake-5.0.0-beta2-windows.zip",
+              "posix":"https://github.com/premake/premake-core/releases/download/v5.0.0-beta2/premake-5.0.0-beta2-linux.tar.gz"}
 
 
 if not os.path.exists(Defines.premakeDir):
+    extension = ".tar.gz" if os.name == "posix" else ".zip"
     if not os.path.exists(tempPremakeArchive):
         print("Downloading Premake")
-        Utils.downloadFile(premakeURL[os.name], tempPremakeArchive)
+        Utils.downloadFile(premakeURL[os.name], tempPremakeArchive+extension)
     print("Extracting Premake")
-    with zipfile.ZipFile(tempPremakeArchive, 'r') as zip_ref:
-        zip_ref.extractall(Defines.premakeDir)
+    if extension == ".tar.gz":
+        with tarfile.open(tempPremakeArchive+extension, 'r:gz') as tar_ref:
+            tar_ref.extractall(Defines.premakeDir)
+    else:
+        with zipfile.ZipFile(tempPremakeArchive+extension, 'r') as zip_ref:
+            zip_ref.extractall(Defines.premakeDir)
     print("Cleaning up")
-    os.remove(tempPremakeArchive)
+    os.remove(tempPremakeArchive+extension)
