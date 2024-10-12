5e1b8ae398f074fb7755872e88607cb2179fccc0
Gints <gintskristaps13@gmail.com>
2024-10-13 00:47:10 +0300

    fix git repo fatal not a git repo

--- Editor/Source/main.cpp
+++ Editor/Source/main.cpp
@@ -1,14 +1,21 @@
 #include <Engine.h>
 #include <Core/Window/Window.h>
+#include <Core/GUI/GUIManager.h>
 #include <Core/Input/Input.h>
 #include <Core/Input/MouseCodes.h>
 #include <Core/Logger/Logger.h>
+#include <Core/World/Screen.h>
 
 
 int main()
 {
 
 	TextureGenEngine::Engine engine(new TextureGenEngine::Window(800, 600, "TexGen"));
+	//Make Gui label
+	TextureGenEngine::GUIManager* guiManager = new TextureGenEngine::GUIManager;
+
+
+	TextureGenEngine::Engine::Get()->GetScreen()->SetGuiMananger(guiManager);
 	while (engine.IsRunning())
 	{
 		if (TextureGenEngine::Input::MouseButtonPressed(TextureGenEngine::Mouse::ButtonLeft))
