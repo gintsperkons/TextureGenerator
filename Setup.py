8e050fea0d2c6b71780f82d653176af2f5b5c35a
Gints <gintskristaps13@gmail.com>
2024-11-22 17:58:54 +0200

    updated to create run file

--- Setup.py
+++ Setup.py
@@ -8,3 +8,14 @@ if __name__ == "__main__":
     Utils.extract("Vendor/Temp/roboto-mono.zip","Vendor/resources/fonts/")
     if (os.path.exists("Vendor/Temp/")):
         shutil.rmtree("Vendor/Temp/")
+
+    if os.name == 'nt':
+        with open("run.bat", "w") as file:
+            file.write(
+            "@echo off\n"
+            "pushd .\\Binaries\\runtime-windows-x86_64-Debug\\\n"
+            "if %errorlevel%==0 (\n"
+            "    start Editor.exe\n"
+            ")\n"
+            "popd\n"
+            )
